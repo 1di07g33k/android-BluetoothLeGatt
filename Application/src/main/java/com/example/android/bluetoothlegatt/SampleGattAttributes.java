5756d092415dea1eb3a21cfe85b06ec53ae61597
1di07g33k <khanajmal165@gmail.com>
2018-03-29 17:18:23 +0530

    Update SampleGattAttributes.java

--- Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -25,6 +25,14 @@ public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_MILI_SERVICE = "0000fee0-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_pair = "0000ff0f-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_CONTROL_POINT = "0000ff05-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_REALTIME_STEPS = "0000ff06-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_ACTIVITY = "0000ff07-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_LE_PARAMS = "0000ff09-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_DEVICE_NAME ="0000ff02-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_CHAR_BATTERY = "0000ff0c-0000-1000-8000-00805f9b34fb";
 
     static {
         // Sample Services.
@@ -33,6 +41,14 @@ public class SampleGattAttributes {
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        attributes.put(UUID_MILI_SERVICE,"UUID MILI Service");
+        attributes.put(UUID_CHAR_pair,"UUID Char Pair");
+        attributes.put(UUID_CHAR_CONTROL_POINT,"UUID Char Control Point");
+        attributes.put(UUID_CHAR_REALTIME_STEPS,"UUID Char Realtime Steps");
+        attributes.put(UUID_CHAR_ACTIVITY,"UUID Char Activity");
+        attributes.put(UUID_CHAR_LE_PARAMS,"UUID Char LE Params");
+        attributes.put(UUID_CHAR_DEVICE_NAME,"UUID Char Device Name");
+        attributes.put(UUID_CHAR_BATTERY,"UUID Char Battery");
     }
 
     public static String lookup(String uuid, String defaultName) {
